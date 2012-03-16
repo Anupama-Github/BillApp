8b38fab3b9c7d59e6c15eae3302fa5ff8f3b05a8
Anupama-Github <anupama.2312.bmsit@gmail.com>
2012-03-17 00:59:52 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BillApp::Application.config.secret_token = 'd88eb6e53380cc1480c6ad7fcba97d5d73acc83e8ff4d60b4980827963a11367ac8304665f65cfa61b3db4d441f1d26dfbacf2afbf5a6cc117f2ef9333c661de'
